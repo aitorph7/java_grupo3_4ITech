e0572b535984c5ec939f265e651d35474c854f66
Aitor Pérez <aitorph7@hotmail.com>
2024-02-10 19:18:47 +0100

    Creo la rama User en la que trabajaré; creo el package 'Model' e incluyo dentro las clases 'User', 'Address' y el enum 'UserRole'.

---
+++ src/main/java/com/escuadronSuicida/backend/model/UserRole.java
@@ -0,0 +1,9 @@
+package com.escuadronSuicida.backend.model;
+
+public enum UserRole {
+    /*
+     En este enum declaro el rol del usuario: asistente
+     al festival o presentador en el mismo.
+     */
+    ATTENDEE, SPEAKER
+}
