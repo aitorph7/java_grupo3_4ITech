9a0fb5fd5b947f67f14673e91183356bd0c46304
Aitor Pérez <aitorph7@hotmail.com>
2024-02-12 17:17:06 +0100

    Corrijo la versión de la rama 'User' de Java de la 17 a la 21, porque había inciado el proyecto con la 17 por error.

--- src/main/java/com/escuadronSuicida/backend/model/User.java
+++ src/main/java/com/escuadronSuicida/backend/model/User.java
@@ -14,6 +14,7 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    private String phone;
     private String password;
     private Address address;
     private UserRole userRole;
--- src/main/java/com/escuadronSuicida/backend/model/UserRole.java
+++ src/main/java/com/escuadronSuicida/backend/model/UserRole.java
@@ -2,8 +2,9 @@ package com.escuadronSuicida.backend.model;
 
 public enum UserRole {
     /*
-     En este enum declaro el rol del usuario: asistente
-     al festival o presentador en el mismo.
+     En este enum declaro el rol del usuario: adminsitrador
+     de la aplicación, asistente al festival o presentador
+     en el mismo.
      */
-    ATTENDEE, SPEAKER
+    ADMIN, ATTENDEE, SPEAKER
 }
