559de1508701126e138d576db571c00ca2447022
Francisco Javier Baeza-Rojano Ossorio <148579168+JavierBRO@users.noreply.github.com>
2024-02-12 19:07:25 +0100

    Creacion Entidades Clase Lunes 12_02_2024

---
+++ src/main/java/com/escuadronSuicida/backend/model/TicketOrderBuy.java
@@ -0,0 +1,29 @@
+package com.escuadronSuicida.backend.model;
+
+import java.time.LocalDateTime;
+
+// entidad intermedia entre User y Ticket, refleja la compra de un ticket
+public class TicketOrderBuy {
+
+    private Long id;
+
+    private LocalDateTime date;
+
+    private Double discount;
+
+    private Double totalPrice;
+
+    private Integer quantity; // comparar este dato a Ticket maxNum
+
+    private String paymentMethod;
+
+    private String channel; // canal de compra: ONLINE, OFFLINE
+
+    private String qrUrl; // Código QR
+
+    // Many To One
+    private User user;
+
+    // Many To One
+    private Ticket ticket;
+}
