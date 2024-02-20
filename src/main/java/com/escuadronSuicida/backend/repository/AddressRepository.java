894088f26d2527718ed757689a20a08a0085867c
Francisco Javier Baeza-Rojano Ossorio <148579168+JavierBRO@users.noreply.github.com>
2024-02-20 17:11:38 +0100

    Eliminada entidad Address

--- src/main/java/com/escuadronSuicida/backend/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.escuadronSuicida.backend.repository;
-
-import com.escuadronSuicida.backend.models.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
