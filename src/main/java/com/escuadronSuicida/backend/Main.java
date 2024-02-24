83636c9be02d1df41cb4e658ee3f2aa32aaacd18
SilviaBargaz <holabargaz@gmail.com>
2024-02-24 19:40:35 +0100

    dejo comentado mis datos y continuaré con los avances de los demás.

--- src/main/java/com/escuadronSuicida/backend/Main.java
+++ src/main/java/com/escuadronSuicida/backend/Main.java
@@ -1,16 +1,34 @@
 package com.escuadronSuicida.backend;
 
+import com.escuadronSuicida.backend.models.Ticket;
+import com.escuadronSuicida.backend.models.TicketOrderBuy;
+import com.escuadronSuicida.backend.repository.TicketOrderBuyRepository;
+import com.escuadronSuicida.backend.repository.TicketRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		ApplicationContext context = SpringApplication.run(Main.class, args);
+
+		TicketRepository ticketRepo = context.getBean(TicketRepository.class);
+		TicketOrderBuyRepository ticketOrderBuyRepo = context.getBean(TicketOrderBuyRepository.class);
+
+	/*	Ticket ticket1 = new Ticket(null, "Ticket1", 50.0, 10);
+		Ticket ticket2 = new Ticket(null, "Ticket1", 50.0, 10);
+		Ticket ticket3 = new Ticket(null, "Ticket1", 50.0, 10);
+		ticketRepo.saveAll(List.of(Ticket1, Ticket2, Ticket3));
+
+		TicketOrderBuy ticketOrderBuy1 = new TicketOrderBuy(null, LocalDateTime.now(), 20.0, 50.0,10,"Tarjeta credito","ONLINE","CodigoqrUrl");
+		TicketOrderBuy ticketOrderBuy2 = new TicketOrderBuy(null, LocalDateTime.now(), 20.0, 50.0,10,"Tarjeta debito ","OFFLINE","CodigoqrUrl");
+		TicketOrderBuy ticketOrderBuy3 = new TicketOrderBuy(null, LocalDateTime.now(), 20.0, 50.0,10,"Tarjeta credito","ONLINE","CodigoqrUrl");
+		ticketOrderBuyRepo.saveAll(List.of(TicketOrderBuy1, TicketOrderBuy2, TicketOrderBuy3));*/
+
 	}
-
-
-
-
 }
