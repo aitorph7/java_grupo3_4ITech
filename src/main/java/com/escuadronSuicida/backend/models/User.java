126ea21f21b3e37d6c4b7c7033d6c4c3a02f7b72
Aitor Pérez <148784736+aitorph7@users.noreply.github.com>
2024-02-25 14:51:01 +0100

    actualizo entidad User.java
    Así es como se encuentra la  entidad User en este momento.
    Domingo 25 Feb 15:00h

--- src/main/java/com/escuadronSuicida/backend/models/User.java
+++ src/main/java/com/escuadronSuicida/backend/models/User.java
@@ -1,42 +1,44 @@
 package com.escuadronSuicida.backend.models;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-@Entity
-
-@Table(name = "users")
+@Entity /* Esta clase es una entidad y debe ser mapeada a una tabla
+en la BD.
+*/
+@Table(name = "users") // desde JPA tb, aunque opcional; la añado para verla.
 public class User {
-    @Id
+    @Id // Especifica la clave primaria de esta entidad.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String firstName;
     private String lastName;
 
+    @Column(unique = true) // el email de cada usuario debe ser único.
     private String email;
-//    @JsonIgnore
-    private String password;
     private String phone;
-    private String address;
 
-    // Opción 1: Esto puede ser una entidad Role y aquí una asociación oneToMany
-    // private List<String> roles = new ArrayList<>();
+    @Column(unique = true) // el nombre de usuario de cada User también debe ser único.
+    private String userName;
 
+    @JsonIgnore // no muestra el dato sensible 'password' en las respuestas JSON.
+    private String password;
 
-    // Opción 2: una enum
-    // @ElementCollection(fetch = FetchType.EAGER)
+    private String address; /* No creo otra entidad Address, solo relleno este
+    atributo con la dirección completa con formato:
+    calle/número/código postal/ciudad/provincia.
+    */
 
     @Enumerated(EnumType.STRING)
-    private UserRole role;
+    private UserRole userRole;
 
-
-
-    // Opción 3: una clase
-    // private UserRole role;
+    @OneToMany(mappedBy = "opinion", cascade = CascadeType.ALL)
+    private List<Comment> comments;
 }
