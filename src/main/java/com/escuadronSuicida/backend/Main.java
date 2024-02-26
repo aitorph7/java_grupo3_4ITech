923d42232afb2cc98b513e731d837954981f7732
Francisco Javier Baeza-Rojano Ossorio <148579168+JavierBRO@users.noreply.github.com>
2024-02-26 12:54:35 +0100

    Datos demo en prueba en main

--- src/main/java/com/escuadronSuicida/backend/Main.java
+++ src/main/java/com/escuadronSuicida/backend/Main.java
@@ -2,8 +2,10 @@ package com.escuadronSuicida.backend;
 
 import com.escuadronSuicida.backend.models.Ticket;
 import com.escuadronSuicida.backend.models.TicketOrderBuy;
+import com.escuadronSuicida.backend.models.Track;
 import com.escuadronSuicida.backend.repository.TicketOrderBuyRepository;
 import com.escuadronSuicida.backend.repository.TicketRepository;
+import com.escuadronSuicida.backend.repository.TrackRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -30,6 +32,31 @@ public class Main {
 		TicketOrderBuy ticketOrderBuy2 = new TicketOrderBuy(null, LocalDateTime.now(), 20.0, 50.0,10,"Tarjeta debito ","OFFLINE","CodigoqrUrl");
 		TicketOrderBuy ticketOrderBuy3 = new TicketOrderBuy(null, LocalDateTime.now(), 20.0, 50.0,10,"Tarjeta credito","ONLINE","CodigoqrUrl");
 		ticketOrderBuyRepo.saveAll(List.of(TicketOrderBuy1, TicketOrderBuy2, TicketOrderBuy3));*/
+
+	/*	TrackRepository trackRepository = context.getBean(TrackRepository.class);
+
+		Track tr1 = new Track(1L,"Track 1", LocalDateTime.of(2024,5,29,9,0), LocalDateTime.of(2024,5,29,10,0));
+		Track tr2 = new Track(2L,"Track 2", LocalDateTime.of(2024,5,29,10,0), LocalDateTime.of(2024,5,29,11,0));
+		Track tr3 = new Track(3L,"Track 3", LocalDateTime.of(2024,5,29,11,0), LocalDateTime.of(2024,5,29,11,30));
+		Track tr4 = new Track(4L,"Track 4", LocalDateTime.of(2024,5,29,11,30), LocalDateTime.of(2024,5,29,12,30));
+		Track tr5 = new Track(5L,"Track 5", LocalDateTime.of(2024,5,29,12,30), LocalDateTime.of(2024,5,29,13,30));
+		Track tr6 = new Track(6L,"Track 6", LocalDateTime.of(2024,5,29,13,30), LocalDateTime.of(2024,5,29,15,30));
+		Track tr7 = new Track(7L,"Track 7", LocalDateTime.of(2024,5,29,15,30), LocalDateTime.of(2024,5,29,16,30));
+		Track tr8 = new Track(8L,"Track 8", LocalDateTime.of(2024,5,29,16,30), LocalDateTime.of(2024,5,29,17,30));
+		Track tr9 = new Track(9L,"Track 9", LocalDateTime.of(2024,5,29,17,30), LocalDateTime.of(2024,5,29,18,0));
+
+		Track tr10 = new Track(10L, "Track 1", LocalDateTime.of(2024,5,30,9,0), LocalDateTime.of(2024,5,30,10,0));
+		Track tr11 = new Track(11L, "Track 2", LocalDateTime.of(2024,5,30,10,0), LocalDateTime.of(2024,5,30,11,0));
+		Track tr12 = new Track(12L, "Track 3", LocalDateTime.of(2024,5,30,11,0), LocalDateTime.of(2024,5,30,11,30));
+		Track tr13 = new Track(13L, "Track 4", LocalDateTime.of(2024,5,30,11,30), LocalDateTime.of(2024,5,30,12,30));
+		Track tr14 = new Track(14L, "Track 5", LocalDateTime.of(2024,5,30,12,30), LocalDateTime.of(2024,5,30,13,30));
+		Track tr15 = new Track(15L, "Track 6", LocalDateTime.of(2024,5,30,13,30), LocalDateTime.of(2024,5,30,15,30));
+		Track tr16 = new Track(16L, "Track 7", LocalDateTime.of(2024,5,30,15,30), LocalDateTime.of(2024,5,30,16,30));
+		Track tr17 = new Track(17L, "Track 8", LocalDateTime.of(2024,5,30,16,30), LocalDateTime.of(2024,5,30,17,30));
+		Track tr18 = new Track(18L, "Track 9", LocalDateTime.of(2024,5,30,17,30), LocalDateTime.of(2024,5,30,18,0));
+
+		trackRepository.saveAll(List.of(tr1, tr2, tr3, tr4, tr5, tr6, tr7, tr8, tr9, tr10, tr11, tr12, tr13,tr14,tr15,tr16,tr17,tr18));*/
+
 
 	}
 }
