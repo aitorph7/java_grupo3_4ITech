5d3400c646249c2f727209bb01af22ad99720a39
angelmaria <angelmaria75@gmail.com>
2024-03-13 02:00:04 +0100

    cambios en KeynoteController.java para que acepte en el url keynotes en lugar de keynote. Cambios en los datos "dummy" exclusivamente de las descripciones de las charlas keynotes

--- src/main/java/com/escuadronSuicida/backend/controller/KeynoteController.java
+++ src/main/java/com/escuadronSuicida/backend/controller/KeynoteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("keynote")
+@RequestMapping("keynotes")
 @AllArgsConstructor
 @CrossOrigin("*")
 
