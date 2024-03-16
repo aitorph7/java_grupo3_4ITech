ec753466fe4a2e028c914c6590a632f4a4703626
angelmaria <angelmaria75@gmail.com>
2024-03-16 13:02:03 +0100

    cambios en KeynoteService y KeynoteServiceImpl para completar según lo puesto en Track por Javi

--- src/main/java/com/escuadronSuicida/backend/services/KeynoteService.java
+++ src/main/java/com/escuadronSuicida/backend/services/KeynoteService.java
@@ -1,6 +1,7 @@
 package com.escuadronSuicida.backend.services;
 
 import com.escuadronSuicida.backend.models.Keynote;
+import com.escuadronSuicida.backend.models.Track;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +16,10 @@ public interface KeynoteService {
 
     Keynote findById(Long id);
 
+    Keynote createKeynote(Keynote keynote);
+
+    Keynote updateKeynote(Long id, Keynote keynote);
+
+    boolean deleteKeynote(Long id);
 
 }
--- src/main/java/com/escuadronSuicida/backend/services/KeynoteServiceImpl.java
+++ src/main/java/com/escuadronSuicida/backend/services/KeynoteServiceImpl.java
@@ -1,26 +1,74 @@
 package com.escuadronSuicida.backend.services;
 
 import com.escuadronSuicida.backend.models.Keynote;
+import com.escuadronSuicida.backend.models.Track;
 import com.escuadronSuicida.backend.repository.KeynoteRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class KeynoteServiceImpl implements KeynoteService {
 
-    private final KeynoteRepository repo;
+    private final KeynoteRepository keynoteRepository;
 
 
     @Override
     public List<Keynote> findKeynote() {
-        return repo.findAll();
+        return keynoteRepository.findAll();
     }
 
     @Override
     public Keynote findById(Long id) {
-        return repo.findById(id).orElseThrow();
+        return keynoteRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Keynote createKeynote(Keynote keynote) {
+        Logger logger = LoggerFactory.getLogger(Keynote.class);
+        try {
+            return keynoteRepository.save(keynote);
+        } catch (Exception e) {
+            // Registrar el error
+            logger.error("Hubo un error al crear el keynote", e);
+
+            // Lanzar una nueva excepción
+            throw new RuntimeException("Hubo un error al crear el keynote: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Keynote updateKeynote(Long id, Keynote keynote) {
+        Optional<Keynote> keynoteOptional = keynoteRepository.findById(id);
+        if (keynoteOptional.isPresent()) {
+            Keynote keynoteFromDatabase = keynoteOptional.get();
+            keynoteFromDatabase.setTitle(keynote.getTitle());
+            keynoteFromDatabase.setSummary(keynote.getSummary());
+            keynoteFromDatabase.setDescription(keynote.getDescription());
+            keynoteFromDatabase.setWebinarUrl(keynote.getWebinarUrl());
+ /*           keynoteFromDatabase.setStartDate(keynote.getStartDate());
+            trackFromDatabase.setEndDate(keynote.getEndDate()); */
+            return keynoteRepository.save(keynoteFromDatabase);
+
+            // esto es actualizar solo esos atributos como sea necesario, aunque en este caso son todos.
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean deleteKeynote(Long id) {
+        Optional<Keynote> keynoteOptional = keynoteRepository.findById(id);
+        if (keynoteOptional.isPresent()) {
+            keynoteRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
