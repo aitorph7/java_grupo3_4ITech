64d3a01f861879689285151dd483aac8684daddc
Angel Martinez <angelmaria75@gmail.com>
2024-04-08 12:27:18 +0200

    añado al UserController.java el login mediante url users/login y Token

--- src/main/java/com/escuadronSuicida/backend/controller/UserController.java
+++ src/main/java/com/escuadronSuicida/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.escuadronSuicida.backend.controller;
 
 import com.escuadronSuicida.backend.dto.Login;
 import com.escuadronSuicida.backend.dto.Register;
+import com.escuadronSuicida.backend.dto.Token;
 import com.escuadronSuicida.backend.models.User;
 import com.escuadronSuicida.backend.models.UserRole;
 import com.escuadronSuicida.backend.repository.UserRepository;
@@ -79,48 +80,47 @@ public class UserController {
         this.userRepository.save(user);
     }
 
-//    @PostMapping("users/login")
-//    public Token login(@RequestBody Login login) {
-//
-//        // Si el email no existe entonces no se hace login
-//        if (!this.userRepository.existsByEmail(login.email())) {
-//            throw new NoSuchElementException("User not found");
-//        }
-//
+    @PostMapping("users/login")
+    public Token login(@RequestBody Login login) {
+
+        // Si el email no existe entonces no se hace login
+        if (!this.userRepository.existsByEmail(login.email())) {
+            throw new NoSuchElementException("User not found");
+      }
+
 //        // Recuperar usuario
-//        User user = this.userRepository.findByEmail(login.email()).orElseThrow();
-//
-//        // Comparar contraseñas
-//        // TODO cuando la contraseña esté cifrada cambiar el proceso de comparación
-//        if (!user.getPassword().equals(login.password())){
-//            throw new RuntimeException("Credenciales incorrectas");
-//        }
-//
-//
-//        // JWT Json Web Token: jwt.io
-//        // Generar token de acceso: eyJhbGciOiJIUzI1NiIsIn......
-//        // Generar el token: https://github.com/jwtk/jjwt?tab=readme-ov-file#creating-a-jwt
-//        Date issuedDate = new Date();
-//        long nextWeekMillis = TimeUnit.DAYS.toMillis(7);
-//        Date expirationDate = new Date(issuedDate.getTime() + nextWeekMillis);
-//        byte[] key = Base64.getDecoder().decode("FZD5maIaX04mYCwsgckoBh1NJp6T3t62h2MVyEtdo3w=");
-//
-//        String token = Jwts.builder()
-//                // id del usuario
-//                .subject(String.valueOf(user.getId()))
-//                // La clave secreta para firmar el token y saber que es nuestro cuando lleguen las peticiones del frontend
-//                .signWith(Keys.hmacShaKeyFor(key))
-//                // Fecha emisión del token
-//                .issuedAt(issuedDate)
-//                // Fecha de expiración del token
-//                .expiration(expirationDate)
-//                // información personalizada: rol, username, email, avatar
-//                .claim("role", user.getRole())
-//                .claim("email", user.getEmail())
-//                // claim("avatar", user.getAvatarUrl())
-//                // Construye el token
-//                .compact();
-//
-//        return new Token(token);
-//    }
+        User user = this.userRepository.findByEmail(login.email()).orElseThrow();
+
+        // Comparar contraseñas
+        // TODO cuando la contraseña esté cifrada cambiar el proceso de comparación
+        if (!user.getPassword().equals(login.password())){
+            throw new RuntimeException("Credenciales incorrectas");
+        }
+
+        // JWT Json Web Token: jwt.io
+        // Generar token de acceso: eyJhbGciOiJIUzI1NiIsIn......
+        // Generar el token: https://github.com/jwtk/jjwt?tab=readme-ov-file#creating-a-jwt
+        Date issuedDate = new Date();
+        long nextWeekMillis = TimeUnit.DAYS.toMillis(7);
+        Date expirationDate = new Date(issuedDate.getTime() + nextWeekMillis);
+        byte[] key = Base64.getDecoder().decode("FZD5maIaX04mYCwsgckoBh1NJp6T3t62h2MVyEtdo3w=");
+
+        String token = Jwts.builder()
+                // id del usuario
+                .subject(String.valueOf(user.getId()))
+                // La clave secreta para firmar el token y saber que es nuestro cuando lleguen las peticiones del frontend
+                .signWith(Keys.hmacShaKeyFor(key))
+                // Fecha emisión del token
+                .issuedAt(issuedDate)
+                // Fecha de expiración del token
+                .expiration(expirationDate)
+                // información personalizada: rol, username, email, avatar
+                .claim("role", user.getUserRole())
+                .claim("email", user.getEmail())
+                // claim("avatar", user.getAvatarUrl())
+                // Construye el token
+                .compact();
+
+        return new Token(token);
+    }
 }
