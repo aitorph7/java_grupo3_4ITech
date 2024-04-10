8ef9fe60cccaa4ed74b15fd0db42bac5a2ed441c
Angel Martinez <angelmaria75@gmail.com>
2024-04-10 17:58:21 +0200

    añadimos -comentado- el código para protección de rutas en SecurityConfig para las versiones nuevas de Spring > 6.1

--- src/main/java/com/escuadronSuicida/backend/Security/SecurityConfig.java
+++ src/main/java/com/escuadronSuicida/backend/Security/SecurityConfig.java
@@ -1,18 +1,36 @@
 package com.escuadronSuicida.backend.Security;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@AllArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-    //    Personalizer el objeto HttpSecurity de Spring Scurity para utilizar nuestro filto JWT y proteger controladores
+    private final RequestJWTFilter jwtFilter;
 
+    // Personalizer el objeto HttpSecurity de Spring Scurity para utilizar nuestro filto JWT y proteger controladores
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        // Para nuevas versiones de Spring > 6.1
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+//                        .requestMatchers("/users/login").permitAll()
+//                        .requestMatchers("/users/register").permitAll()
+//                        .anyRequest().authenticated()
+//                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+//                .build();
+
+        // Para lase versiones anteriores a Spring 6.1
         // Sin estados, sin sesiones Http, ya que usamos tokens JWT
         // La autenticación JWT es sin estado y no depende de sesiones o cookies
         http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -26,7 +44,7 @@ public class SecurityConfig {
                 .authenticated();
 
         // Asignar nuesro filtro personalizado de JWT
-//        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
