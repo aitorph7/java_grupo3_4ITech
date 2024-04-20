3b0b5195072f4a6fd61d7226a96068459fc6b72c
SilviaBargaz <holabargaz@gmail.com>
2024-04-20 13:03:00 +0200

    Listado TObuy Descendente

--- src/main/java/com/escuadronSuicida/backend/controller/TicketOrderBuyController.java
+++ src/main/java/com/escuadronSuicida/backend/controller/TicketOrderBuyController.java
@@ -32,6 +32,11 @@ public class TicketOrderBuyController {
         return ResponseEntity.ok(ticketOrderBuy);
     }
 
+    @GetMapping("ticketOrderBuys/filter-by-ticketOrderBuy/{id}")
+    public List<TicketOrderBuy> findAllByTicketOrderBuyId(@PathVariable Long id) {
+        return this.repo.findByTicketOrderBuy_IdOrderByIdDesc(id);
+    }
+
 
     private TicketOrderBuyRepository repo;
 
--- src/main/java/com/escuadronSuicida/backend/repository/TicketOrderBuyRepository.java
+++ src/main/java/com/escuadronSuicida/backend/repository/TicketOrderBuyRepository.java
@@ -3,5 +3,9 @@ package com.escuadronSuicida.backend.repository;
 import com.escuadronSuicida.backend.models.TicketOrderBuy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketOrderBuyRepository extends JpaRepository<TicketOrderBuy, Long> {
+
+    List<TicketOrderBuy> findByTicketOrderBuy_IdOrderByIdDesc(Long id);
 }
