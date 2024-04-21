19b67a57ff28ba781cae09201d327f728f98c500
Aitor Pérez <aitorph7@hotmail.com>
2024-04-21 02:29:31 +0200

    en 'Main.java' corrijo los datos demo de usuarios creados con .builder() y passwordEncoder para evitar los errores al logear encontrados hoy Sábado + elimino 'litte Room' + errores ortográficos --- en 'UserController.java' dejo comentado un método DELETE condicionado al rol del usuario + añado excepciones personalizadas.

--- src/main/java/com/escuadronSuicida/backend/models/UserRole.java
+++ src/main/java/com/escuadronSuicida/backend/models/UserRole.java
@@ -1,11 +1,9 @@
 package com.escuadronSuicida.backend.models;
 
+/* En este enum declaro el rol del usuario: administrador
+de la aplicación o asistente. */
+
 public enum UserRole {
-    /*
-     En este enum declaro el rol del usuario: administrador
-     de la aplicación, asistente al festival o presentador
-     de alguna charla/keynote.
-     */
     USER,
     ADMIN
     //SPEAKER a futuros en siguientes iteraciones
